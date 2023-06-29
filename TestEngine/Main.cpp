1162d4ef7175ec1b87a36fe55b8f5410547c507c
sametydev <108689707+sametydev@users.noreply.github.com>
2023-06-29 18:16:14 +0300

    Init DirectXMath for Temporary Prototyping
    Im adding DirectXMath for temporary solution, I'm gonna use my own math library from my NullEngine, Its for temporary and prototyping!

--- TestEngine/Main.cpp
+++ TestEngine/Main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <iostream>
+#include <ostream>
+#include <windows.h>
 
 #pragma once
 #include <d3d11_1.h>
 #include <d3dcompiler.h>
 #include <dxgi1_3.h>
+#include <DirectXMath.h>
 
 
 #pragma comment(lib,"d3d11.lib")
@@ -15,10 +18,29 @@
 #pragma comment(lib,"dxguid.lib")
 #endif
 
+using namespace DirectX;
+using namespace std;
+
+ostream& operator<<(ostream& os, FXMVECTOR v)
+{
+	XMFLOAT3 dest;
+	XMStoreFloat3(&dest, v);
+	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	return os;
+}
 
 
 int main() {
-
+	
 	std::cout << "TestEngine Init" << "\n";
+
+	cout.setf(ios_base::boolalpha);
+	
+	if (!XMVerifyCPUSupport())
+	{
+		cout << "DirectX math not supported" << endl;
+		return 0;
+	}
+
 	return -1;
 }
