0d62073848824949e6672fc47e9b400b1b43827d
sametydev <108689707+sametydev@users.noreply.github.com>
2023-06-29 18:09:55 +0300

    Temporary DX things

--- TestEngine/Main.cpp
+++ TestEngine/Main.cpp
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <iostream>
+
+#pragma once
+#include <d3d11_1.h>
+#include <d3dcompiler.h>
+#include <dxgi1_3.h>
+
+
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib,"DXGI.lib")
+
+#ifdef _DEBUG
+#pragma comment(lib,"dxguid.lib")
+#endif
+
+
+
 int main() {
 
 	std::cout << "TestEngine Init" << "\n";
