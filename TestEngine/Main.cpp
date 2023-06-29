b93e98d94467e71055837fb4115b755874a5b86a
sametydev <108689707+sametydev@users.noreply.github.com>
2023-06-29 18:22:13 +0300

    Cleanup

--- TestEngine/Main.cpp
+++ TestEngine/Main.cpp
@@ -2,30 +2,13 @@
 #include <iostream>
 #include <ostream>
 #include <windows.h>
+#include <TestEngine/DXConfig.h>
 
-#pragma once
-#include <d3d11_1.h>
-#include <d3dcompiler.h>
-#include <dxgi1_3.h>
-#include <DirectXMath.h>
-
-
-#pragma comment(lib,"d3d11.lib")
-#pragma comment(lib,"d3dcompiler.lib")
-#pragma comment(lib,"DXGI.lib")
-
-#ifdef _DEBUG
-#pragma comment(lib,"dxguid.lib")
-#endif
-
-using namespace DirectX;
-using namespace std;
-
-ostream& operator<<(ostream& os, FXMVECTOR v)
+std::ostream& operator<<(std::ostream& os, FXMVECTOR v)
 {
 	XMFLOAT3 dest;
 	XMStoreFloat3(&dest, v);
-	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	os << "Vec (" << dest.x << ", " << dest.y << ", " << dest.z << ")";
 	return os;
 }
 
@@ -33,14 +16,7 @@ ostream& operator<<(ostream& os, FXMVECTOR v)
 int main() {
 	
 	std::cout << "TestEngine Init" << "\n";
-
-	cout.setf(ios_base::boolalpha);
 	
-	if (!XMVerifyCPUSupport())
-	{
-		cout << "DirectX math not supported" << endl;
-		return 0;
-	}
 
 	return -1;
 }
