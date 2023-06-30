8f7e3e3717f9b6ae9d0f22ca3bfbf0f2179b23fc
sametydev <108689707+sametydev@users.noreply.github.com>
2023-06-30 03:55:23 +0300

    Some stuffss.

--- TestEngine/Main.cpp
+++ TestEngine/Main.cpp
@@ -4,9 +4,105 @@
 #include <windows.h>
 #include <TestEngine/DXConfig.h>
 using namespace std;
+#pragma warning (disable:26495)
+
+LRESULT CALLBACK WndProc(HWND hwnd,
+	UINT msg,
+	WPARAM wParam,
+	LPARAM lParam)
+{
+	switch (msg)
+	{
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) {
+			if (MessageBox(0, L"Are you sure you want to exit?",
+				L"Really?", MB_YESNO | MB_ICONQUESTION) == IDYES)
+				DestroyWindow(hwnd);
+		}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd,
+		msg,
+		wParam,
+		lParam);
+}
+HWND hwnd;
+
+bool InitializeWindow(HINSTANCE hInstance,
+	int ShowWnd,
+	int width, int height,
+	bool fullscreen)
+{
+	if (fullscreen)
+	{
+		HMONITOR hmon = MonitorFromWindow(hwnd,
+			MONITOR_DEFAULTTONEAREST);
+		MONITORINFO mi = { sizeof(mi) };
+		GetMonitorInfo(hmon, &mi);
+
+		width = mi.rcMonitor.right - mi.rcMonitor.left;
+		height = mi.rcMonitor.bottom - mi.rcMonitor.top;
+	}
+
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = L"Main";
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, L"Error registering class",
+			L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+
+	hwnd = CreateWindowEx(NULL,
+		L"Main",
+		L"Main",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		width, height,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	if (!hwnd)
+	{
+		MessageBox(NULL, L"Error creating window",
+			L"Error", MB_OK | MB_ICONERROR);
+		return false;
+	}
+	if (fullscreen)
+	{
+		SetWindowLong(hwnd, GWL_STYLE, 0);
+	}
+
+	ShowWindow(hwnd, ShowWnd);
+	UpdateWindow(hwnd);
+
+	return true;
+}
 
 
 int main() {
+	InitializeWindow(0, 1, 800, 600, 0);
+	MSG msg{};
 
 
 	///
@@ -91,7 +187,7 @@ int main() {
 	fd.Windowed = TRUE;
 
 	//Not Working Yet! Because We dont have a Window Class
-	HR(dxgiFactory2->CreateSwapChainForHwnd(m_d3dDevice, nullptr, &sd, &fd, nullptr,&mSwapChain));
+	HR(dxgiFactory2->CreateSwapChainForHwnd(m_d3dDevice, hwnd, &sd, &fd, nullptr,&mSwapChain));
 
 	SAFE_RELEASE(dxgiFactory2);
 	SAFE_RELEASE(dxgiFactory1);
@@ -108,9 +204,58 @@ int main() {
 	
 	SAFE_RELEASE(backBuffer);
 
-	
-
 	std::cout << "TestEngine Init" << "\n";
+
+	D3D11_TEXTURE2D_DESC depthStencilDesc;
+	depthStencilDesc.Width = 800;
+	depthStencilDesc.Height = 600;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.ArraySize = 1;
+	depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+	if (mIsMsaaEnable)
+	{
+		depthStencilDesc.SampleDesc.Count = 4;
+		depthStencilDesc.SampleDesc.Quality = m4xMsaaQuality - 1;
+	}
+	else
+	{
+		depthStencilDesc.SampleDesc.Count = 1;
+		depthStencilDesc.SampleDesc.Quality = 0;
+	}
+
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilDesc.CPUAccessFlags = 0;
+	depthStencilDesc.MiscFlags = 0;
+
+	ID3D11Texture2D* mDepthStencilBuffer;
+	ID3D11DepthStencilView* mDepthStencilView;
+
+	HR(m_d3dDevice->CreateTexture2D(
+		&depthStencilDesc,
+		0,
+		&mDepthStencilBuffer));
+	HR(m_d3dDevice->CreateDepthStencilView(
+		mDepthStencilBuffer,
+		0,
+		&mDepthStencilView));
+
+
+	m_d3dContext->OMSetRenderTargets(
+		1, &mRenderTargetView, mDepthStencilView);
+
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
 	return -1;
 }
