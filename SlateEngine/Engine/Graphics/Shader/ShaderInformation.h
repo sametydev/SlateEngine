59e373884fe8bd55846bc204a2957c04b5d2e782
sametydev <108689707+sametydev@users.noreply.github.com>
2023-09-06 00:09:48 +0300

    Update ShaderInformation.h

--- SlateEngine/Engine/Graphics/Shader/ShaderInformation.h
+++ SlateEngine/Engine/Graphics/Shader/ShaderInformation.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <SimpleIni.h>
+
 struct ShaderInformation
 {
+public:
 	const char* displayName;
 	const char* shaderType;
 	const char* inputLayout;
@@ -9,3 +12,25 @@ struct ShaderInformation
 	const char* csoName;
 };
 
+class ShaderInformationLoader {
+public:
+	static ShaderInformation LoadSInfo(const char* file) {
+		ShaderInformation sinfo{};
+
+		CSimpleIniA ini;
+		ini.SetUnicode();
+
+		SI_Error rc = ini.LoadFile(file);
+		if (rc < 0) { MessageBox(0, L"ShaderInformation file is broken.", L"ShaderInformation", 0); };
+
+		sinfo.csoName     = ini.GetValue("Shader", "CSOName");
+		sinfo.hlslFile    = ini.GetValue("Shader", "HLSLFile");
+		sinfo.entryPoint  = ini.GetValue("Shader", "EntryPoint");
+		sinfo.inputLayout = ini.GetValue("Shader", "InputLayout");
+		sinfo.shaderType  = ini.GetValue("Shader", "ShaderType");
+		sinfo.displayName = ini.GetValue("Shader", "DisplayName");
+
+
+		return sinfo;
+	}
+};
